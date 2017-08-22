c2a8c22a01684b6145b12490763727bed027ec35
sblundy <el_gonzzo@hotmail.com>
2017-08-22 14:33:04 -0400

    Adding initial multi-threading support

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,15 +1,21 @@
 use std::net::{TcpListener};
 use std::path::Path;
+use std::thread::spawn;
+use std::sync::Arc;
 use self::file_system::{FileSystemAdapter};
 use self::handlers::handle_client;
 
 pub fn serve(listener: TcpListener, root: &Path) {
     let fs_adapter = FileSystemAdapter::new(root);
+    let adapter_rc = Arc::new(fs_adapter);
 
-    // accept connections and process them serially
+    // accept connections and process them in separate threads
     for stream_ref in listener.incoming() {
         match stream_ref {
-            Ok(stream) => handle_client(stream, &fs_adapter),
+            Ok(stream) => {
+                let local_rc = adapter_rc.clone();
+                spawn(move || handle_client(stream, local_rc.as_ref()));
+            },
             Err(e) => println!("Error with stream:{}", e)
         }
     }

602d5e371ff69293816c68bea6ac556bfe6209a3
sblundy <el_gonzzo@hotmail.com>
2017-09-04 14:55:59 -0700

    Adding support for HTTPs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::net::{TcpListener};
 use std::path::Path;
 use clap::{Arg, App};
+use std::fs::File;
+use std::io::Read;
+use openssl::pkcs12::{Pkcs12, ParsedPkcs12};
+use openssl::ssl::{SslMethod, SslAcceptorBuilder, SslAcceptor};
+use openssl::pkey::PKey;
+use openssl::x509::X509;
+use openssl::stack::Stack;
 
 extern crate clap;
 extern crate bufstream;
 extern crate chrono;
+extern crate openssl;
 
 mod server;
 
@@ -23,22 +31,109 @@ fn main() {
             .value_name("PORT_NUMBER")
             .default_value("8080")
             .takes_value(true))
+        .arg(Arg::with_name("cert")
+            .long("cert").value_name("CERT_FILE")
+            .takes_value(true))
+        .arg(Arg::with_name("pk")
+            .long("pk").value_name("PRIVATE_KEY_FILE")
+            .takes_value(true))
         .arg(Arg::with_name("root_directory")
             .required(true)
             .value_name("ROOT_DIRECTORY"))
         .get_matches();
 
 
-    let root_path = Path::new(matches.value_of("root_directory").unwrap());
 
-    if !root_path.exists() {
-        eprintln!("Root path does not exist: {}", root_path.display());
-    } else {
-        println!("binding to:{}:{}", matches.value_of("address").unwrap(), matches.value_of("port").unwrap());
-        println!("root={}->{}", matches.value_of("root_directory").unwrap(), root_path.display());
-        match TcpListener::bind(format!("{}:{}", matches.value_of("address").unwrap(), matches.value_of("port").unwrap())) {
-            Ok(listener) => server::serve(listener, root_path),
-            Err(e) => println!("Error on bind:{}", e)
+    match matches.value_of("root_directory") {
+        Some(root_dir) => {
+            let root_path = Path::new(root_dir);
+            let acceptor = match (matches.value_of("cert"), matches.value_of("pk")) {
+                (Some(cert_path),Some(private_key_path)) => Some(create_acceptor(&cert_path, &private_key_path)),
+                (Some(_), None) => panic!("If the cert is specified, so must the private key"),
+                (None, Some(_)) => panic!("If the private key is specified, so must the cert"),
+                (None, None) => None
+            };
+
+            if !root_path.exists() {
+                eprintln!("Root path does not exist: {}", root_path.display());
+            } else {
+                println!("binding to:{}:{}", matches.value_of("address").unwrap(), matches.value_of("port").unwrap());
+                println!("root={}->{}", matches.value_of("root_directory").unwrap(), root_path.display());
+                match TcpListener::bind(format!("{}:{}", matches.value_of("address").unwrap(), matches.value_of("port").unwrap())) {
+                    Ok(listener) => match acceptor {
+                        None => server::serve(listener, root_path),
+                        Some(ac) => server::serve_https(listener, root_path, ac)
+                    },
+                    Err(e) => println!("Error on bind:{}", e)
+                }
+            }
+        },
+        None => eprintln!("The root directory is required")
+    }
+}
+
+fn create_acceptor(cert_path: &str, private_key_path: &str) -> SslAcceptor {
+    let identity_result = load_cert(cert_path, private_key_path);
+
+    match identity_result {
+        Ok((pkey, cert)) => {
+            let chain:Stack<X509> = Stack::new().unwrap();
+            SslAcceptorBuilder::mozilla_intermediate(SslMethod::tls(), &pkey, &cert, &chain)
+                .unwrap()
+                .build()
+        },
+        Err(e) => panic!("Error loading cert:{}", e)
+    }
+}
+
+fn load_cert(cert_path: &str, private_key_path: &str) -> Result<(PKey, X509), String> {
+    let pkey = match File::open(private_key_path) {
+        Ok(mut file) => {
+            let mut pkey_buff: Vec<u8> = vec![];
+            file.read_to_end(&mut pkey_buff).unwrap();
+            match PKey::private_key_from_pem(pkey_buff.as_ref()) {
+                Ok(pkey) => pkey,
+                Err(e) => {
+                    return Err(format!("Error extracting private key:{}", e));
+                }
+            }
+        },
+        Err(e) => {
+            return Err(format!("Error reading private key:{}", e));
         }
+    };
+    let x509 = match File::open(cert_path) {
+        Ok(mut file) => {
+            let mut cert_buff: Vec<u8> = vec![];
+            file.read_to_end(&mut cert_buff).unwrap();
+            match X509::from_pem(cert_buff.as_ref()) {
+                Ok(x509) => x509,
+                Err(e) => {
+                    return Err(format!("Error extracting cert:{}", e));
+                }
+            }
+        },
+        Err(e) => {
+            return Err(format!("Error reading cert:{}", e));
+        }
+    };
+
+    return Ok((pkey, x509));
+}
+
+fn load_pkcs12_cert(path: &str) -> Result<ParsedPkcs12, String> {
+    match File::open(path) {
+        Ok(mut file) => {
+            let mut pkcs12: Vec<u8> = vec![];
+            file.read_to_end(&mut pkcs12).unwrap();
+            match Pkcs12::from_der(&pkcs12) {
+                Ok(pkcs12) => match pkcs12.parse("") {
+                    Ok(identity) => Ok(identity),
+                    Err(_) => Err("Parse failed".to_string())
+                },
+                Err(_) => Err("from_der failed".to_string())
+            }
+        },
+        Err(_) => Err("Failed to open file".to_string())
     }
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpListener};
 use std::path::Path;
 use std::sync::Arc;
+use openssl::ssl::{SslAcceptor};
 use self::file_system::{FileSystemAdapter};
 use self::handlers::handle_client;
 use self::pool::ThreadPool;
@@ -17,7 +18,32 @@ pub fn serve(listener: TcpListener, root: &Path) {
                 let local_rc = adapter_rc.clone();
                 pool.execute(move || handle_client(stream, local_rc.as_ref()));
             },
-            Err(e) => println!("Error with stream:{}", e)
+            Err(e) => eprintln!("Error with stream:{}", e)
+        }
+    }
+}
+
+pub fn serve_https(listener: TcpListener, root: &Path, acceptor: SslAcceptor) {
+    let fs_adapter = FileSystemAdapter::new(root);
+    let adapter_rc = Arc::new(fs_adapter);
+    let acceptor_rc = Arc::new(acceptor);
+    let pool = ThreadPool::new(4);
+
+    // accept connections and process them in separate threads
+    for stream_ref in listener.incoming() {
+        match stream_ref {
+            Ok(connection) => {
+                let local_rc = adapter_rc.clone();
+                let local_acceptor_rc = acceptor_rc.clone();
+                pool.execute(move || {
+                    match local_acceptor_rc.accept(connection) {
+                        Ok(stream) => handle_client(stream, local_rc.as_ref()),
+                        Err(e) => eprintln!("Accept failed:{}", e)
+                    }
+
+                });
+            },
+            Err(e) => eprintln!("Error with stream:{}", e)
         }
     }
 }
